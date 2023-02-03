e192ca578a0e19c45b776fdec5a5dd3f19fb4cd8
Thomas Fröhle <thomas.froehle@generic.de>
2023-02-03 13:00:05 +0100

    reduce complexity

--- source/TodoTree/src/app/app.component.ts
+++ source/TodoTree/src/app/app.component.ts
@@ -9,98 +9,31 @@ import { filter, takeUntil } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  title = 'Angular 14 RxJS 7 - Angular v2 Sample';
-  isIframe = false;
-  loginDisplay = false;
-  private readonly _destroying$ = new Subject<void>();
-
-  constructor(
-    @Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
-    private authService: MsalService,
-    private msalBroadcastService: MsalBroadcastService
-  ) {}
+export class AppComponent implements OnInit {
+  constructor(private authService: MsalService, private msalBroadcastService: MsalBroadcastService) {}
+  isLoggedIn = false;
 
   ngOnInit(): void {
-    this.isIframe = window !== window.parent && !window.opener; // Remove this line to use Angular Universal
-    this.setLoginDisplay();
-
-    this.authService.instance.enableAccountStorageEvents(); // Optional - This will enable ACCOUNT_ADDED and ACCOUNT_REMOVED events emitted when a user logs in or out of another tab or window
-    this.msalBroadcastService.msalSubject$
-      .pipe(
-        filter((msg: EventMessage) => msg.eventType === EventType.ACCOUNT_ADDED || msg.eventType === EventType.ACCOUNT_REMOVED),
-      )
-      .subscribe((result: EventMessage) => {
-        if (this.authService.instance.getAllAccounts().length === 0) {
-          window.location.pathname = "/";
-        } else {
-          this.setLoginDisplay();
-        }
-      });
-
     this.msalBroadcastService.inProgress$
-      .pipe(
-        filter((status: InteractionStatus) => status === InteractionStatus.None),
-        takeUntil(this._destroying$)
-      )
-      .subscribe(() => {
-        this.setLoginDisplay();
-        this.checkAndSetActiveAccount();
-      })
+    .pipe(
+      filter((status: InteractionStatus) => status === InteractionStatus.None),
+    )
+    .subscribe(() => {
+      this.isLoggedIn = this.checkAndSetActiveAccount();
+    })
   }
 
-  setLoginDisplay() {
-    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
-  }
-
-  checkAndSetActiveAccount(){
-    /**
-     * If no active account set but there are accounts signed in, sets first account to active account
-     * To use active account set here, subscribe to inProgress$ first in your component
-     * Note: Basic usage demonstrated. Your app may require more complicated account selection logic
-     */
-    let activeAccount = this.authService.instance.getActiveAccount();
-
-    if (!activeAccount && this.authService.instance.getAllAccounts().length > 0) {
-      let accounts = this.authService.instance.getAllAccounts();
-      this.authService.instance.setActiveAccount(accounts[0]);
+  checkAndSetActiveAccount() : boolean {
+    const activeAccount = this.authService.instance.getActiveAccount();
+    const allAccounts = this.authService.instance.getAllAccounts();
+    if (!activeAccount && allAccounts.length > 0) {
+      this.authService.instance.setActiveAccount(allAccounts[0]);
     }
+
+    return this.authService.instance.getActiveAccount() !== null;
   }
 
-  loginRedirect() {
-    if (this.msalGuardConfig.authRequest){
-      this.authService.loginRedirect({...this.msalGuardConfig.authRequest} as RedirectRequest);
-    } else {
-      this.authService.loginRedirect();
-    }
-  }
-
-  loginPopup() {
-    if (this.msalGuardConfig.authRequest){
-      this.authService.loginPopup({...this.msalGuardConfig.authRequest} as PopupRequest)
-        .subscribe((response: AuthenticationResult) => {
-          this.authService.instance.setActiveAccount(response.account);
-        });
-      } else {
-        this.authService.loginPopup()
-          .subscribe((response: AuthenticationResult) => {
-            this.authService.instance.setActiveAccount(response.account);
-      });
-    }
-  }
-
-  logout(popup?: boolean) {
-    if (popup) {
-      this.authService.logoutPopup({
-        mainWindowRedirectUri: "/"
-      });
-    } else {
-      this.authService.logoutRedirect();
-    }
-  }
-
-  ngOnDestroy(): void {
-    this._destroying$.next(undefined);
-    this._destroying$.complete();
+  login() {
+    this.authService.instance.loginRedirect();
   }
 }
--- source/TodoTree/src/app/app.module.ts
+++ source/TodoTree/src/app/app.module.ts
@@ -7,18 +7,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { IPublicClientApplication, PublicClientApplication, InteractionType, BrowserCacheLocation, LogLevel } from '@azure/msal-browser';
-import { MsalGuard, MsalInterceptor, MsalBroadcastService, MsalInterceptorConfiguration, MsalModule, MsalService, MSAL_GUARD_CONFIG, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG, MsalGuardConfiguration, MsalRedirectComponent } from '@azure/msal-angular';
 import { FailedComponent } from './failed/failed.component';
 import { TodoComponent } from './todo/todo.component';
 import { Client } from '@microsoft/microsoft-graph-client';
 import { AuthCodeMSALBrowserAuthenticationProviderOptions, AuthCodeMSALBrowserAuthenticationProvider } from '@microsoft/microsoft-graph-client/authProviders/authCodeMsalBrowser';
+import { BrowserCacheLocation, InteractionType, LogLevel, PublicClientApplication } from '@azure/msal-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { MsalService, MsalModule, MSAL_INSTANCE, MsalBroadcastService, MsalRedirectComponent } from '@azure/msal-angular';
 
 export function loggerCallback(logLevel: LogLevel, message: string) {
   console.log(message);
@@ -44,26 +40,6 @@ export const publicClientApplication = new PublicClientApplication({
     }
   });
 
-export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
-  const protectedResourceMap = new Map<string, Array<string>>();
-  protectedResourceMap.set('https://graph.microsoft.com/v1.0/me', ["user.read", "tasks.readWrite"]);
-
-  return {
-    interactionType: InteractionType.Redirect,
-    protectedResourceMap
-  };
-}
-
-export function MSALGuardConfigFactory(): MsalGuardConfiguration {
-  return {
-    interactionType: InteractionType.Redirect,
-    authRequest: {
-      scopes: ["user.read", "tasks.readWrite"]
-    },
-    loginFailedRoute: '/login-failed'
-  };
-}
-
 function GraphClient(authService: MsalService, clientApplication: PublicClientApplication): Client {
 
   const options: AuthCodeMSALBrowserAuthenticationProviderOptions = {
@@ -83,15 +59,12 @@ function GraphClient(authService: MsalService, clientApplication: PublicClientAp
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ProfileComponent,
     FailedComponent,
     TodoComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
     MatButtonModule,
     MatToolbarModule,
     MatListModule,
@@ -101,11 +74,6 @@ function GraphClient(authService: MsalService, clientApplication: PublicClientAp
   ],
   providers: [
     {
-      provide: HTTP_INTERCEPTORS,
-      useClass: MsalInterceptor,
-      multi: true
-    },
-    {
       provide: MSAL_INSTANCE,
       useValue: publicClientApplication
     },
@@ -114,20 +82,11 @@ function GraphClient(authService: MsalService, clientApplication: PublicClientAp
       useValue: publicClientApplication
     },
     {
-      provide: MSAL_GUARD_CONFIG,
-      useFactory: MSALGuardConfigFactory
-    },
-    {
-      provide: MSAL_INTERCEPTOR_CONFIG,
-      useFactory: MSALInterceptorConfigFactory
-    },
-    {
       provide : Client,
       useFactory: GraphClient,
       deps: [MsalService, PublicClientApplication]
     },
     MsalService,
-    MsalGuard,
     MsalBroadcastService
   ],
   bootstrap: [AppComponent, MsalRedirectComponent]
